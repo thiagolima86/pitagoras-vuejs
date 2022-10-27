702229bca5c8539207f1f04f30f4656a0cffd823
Thiago Lima <thiagolima86@gmail.com>
2022-10-26 23:51:58 -0300

    Mantem a proporção

--- main.js
+++ main.js
@@ -12,22 +12,36 @@ new Vue({
 			return hipotenusa
 		},
 		triangle(){
+			cat1 = this.cateto1
+			cat2 = this.cateto2
+
+			if(cat1 >= cat2) {
+				prop = cat1/cat2
+				max =200/prop
+				cat1 = prop*max
+				cat2 = max
+			}else{
+				prop = cat2/cat1
+				cat2 = prop*100
+				cat1 = 100
+			}
+
 			var c = document.getElementById("triangle")
 			this.canvas = c.getContext('2d')
 			
 			this.canvas.beginPath();
 			this.canvas.moveTo(0, 0)
-			this.canvas.lineTo(0, this.cateto1)
+			this.canvas.lineTo(0, cat1)
 			this.canvas.stroke()
 			
-			this.canvas.clearRect(0, 0, 250, 200);
+			this.canvas.clearRect(0, 0, 250, 250);
 			
 			this.canvas.moveTo(0, 0)
-			this.canvas.lineTo(this.cateto2, this.cateto1)
+			this.canvas.lineTo(cat2, cat1)
 			this.canvas.stroke()
 			
-			this.canvas.moveTo(0, this.cateto1)
-			this.canvas.lineTo(this.cateto2, this.cateto1)
+			this.canvas.moveTo(0, cat1)
+			this.canvas.lineTo(cat2, cat1)
 			this.canvas.stroke()
 			
 			
